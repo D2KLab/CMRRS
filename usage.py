41596f19b76f35bf9995ff79396290dba39b4e60
links-ai <mediaverse@venus>
2022-01-31 11:02:48 +0100

    port set to 5000

--- usage.py
+++ usage.py
@@ -2,9 +2,10 @@ from flask import json
 import numpy as np
 import requests
 import pdb
+#import time
 
-update_url   = 'http://localhost:6000/mv_retrieval/v0.1/add_content'
-retrieve_url = 'http://localhost:6000/mv_retrieval/v0.1/retrieve_contents'
+update_url   = 'http://localhost:5000/mv_retrieval/v0.1/add_content'
+retrieve_url = 'http://localhost:5000/mv_retrieval/v0.1/retrieve_contents'
 
 if __name__ == '__main__':
     pool  = np.random.rand(10, 512).astype(np.float32)
@@ -15,7 +16,8 @@ if __name__ == '__main__':
     for content in pool:
         random_str = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 10))    
         payload    = {'content_id': random_str, 'embedding': content.tolist()}
-        response   = requests.post(update_url, json=payload)
+        #time.sleep(5)
+        response   = requests.post(update_url,json=payload)
         print('[UPDATE] -- status: {}, {}'.format(response.status_code, response.content))
     payload  = {'query': query.tolist(), 'n_contents_to_retrieve': 4}
     response = requests.post(retrieve_url, json=payload)
