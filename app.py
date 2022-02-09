e3d9480853bd90b4027113f478f5c2813f4e429e
lorenzo bongiovanni <lorenzo.bongiovanni@linksfoundation.com>
2022-02-09 15:49:08 +0100

    created ClipEncoder class & moved encoding from Indexer class to API functions 'add_content' and 'retrieve'

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ Doubts:
 import argparse
 import logging
 import os
-from typing import List, Tuple
+from typing import List, Tuple, Union
 from datetime import datetime
 
 import faiss
@@ -26,34 +26,34 @@ from PIL import Image
 
 app     = Flask(__name__)
 EMB_SIZE = 512
-model, preprocess = clip.load("ViT-B/32") # (or load model starting from state_dict)
 
-app.config['Clip'] = model
-app.config['Preprocess'] = preprocess 
 
-def encode(input):
-    if type(input) is str:
-        text = clip.tokenize(input)
-        embs = app.config['Clip'].encode_text(text)
-    else:
-        image = app.config['Preprocess'](Image.open(input)).unsqueeze(0)
-        embs = app.config['Clip'].encode_image(image)
+class ClipEncoder:
+    def __init__(self) -> None:
+        model, preprocess = clip.load("ViT-B/32") # (or load model starting from state_dict)
+        self.model = model
+        self.preprocess = preprocess
 
-    return embs
+    def encode(self, input: Union[str, np.array]) -> np.array:
+        if isinstance(input, str):
+            text = clip.tokenize(input)
+            return self.model.encode_text(text).astype(np.float32)
+        else:
+            image = self.preprocess(Image.open(input)).unsqueeze(0)
+            return self.model.encode_image(image).astype(np.float32)
+
 
 class Indexer:
-    def __init__(self, emb_size: int=EMB_SIZE):
+    def __init__(self, emb_size: int=EMB_SIZE) -> None:
         # to get total length of flat index: index.xb.size()
         # to get number of embeddings in index: index.xb.size() // EMB_SIZE
         self.index        = faiss.index_factory(emb_size, "Flat", faiss.METRIC_INNER_PRODUCT)
         self.faissId2MVId = []
 
-    def add_content(self, input, content_id: str):  # (input: np.ndarray or str)
+    def add_content(self, content_embedding: np.array, content_id: str) -> None:  # (input: np.ndarray or str)
         """
         :param content_embedding: embedding having shape (N, EMB_SIZE)
         """
-        content_embedding = encode(input)
-
         assert len(content_embedding.shape) == 1, 'Expecting one content at a time'
         assert content_embedding.shape[-1] == EMB_SIZE, 'Expected embedding size of {}, got {}'.format(EMB_SIZE, content_embedding.shape[-1])
         content_embedding = content_embedding.reshape(1, -1)
@@ -61,14 +61,12 @@ class Indexer:
         self.index.add(content_embedding)
         self.faissId2MVId.append(content_id)
 
-    def retrieve(self, input, k: int) -> Tuple[List[str], List[float]]:  # (input: np.ndarray or str)
+    def retrieve(self, query_embedding: np.array, k: int) -> Tuple[List[str], List[float]]:  # (input: np.ndarray or str)
         """
         :param query_embedding: np.ndarray having shape (EMB_SIZE,)
         :param k: retrieve top_k contents from the pool
         """
-        query_embedding = encode(input)
-
-        query_embedding            = query_embedding.reshape(1, -1).astype(np.float32)
+        query_embedding  = query_embedding.reshape(1, -1).astype(np.float32)
         faiss.normalize_L2(query_embedding)
         similarities, contents_idx = self.index.search(query_embedding, k)
         # assuming only one query
@@ -82,7 +80,10 @@ class Indexer:
         
         return mv_content_ids, similarities
 
+
 app.config['Indexer'] = Indexer()
+app.config['ClipEncoder'] = ClipEncoder()
+
 
 @app.route('/mv_retrieval/v0.1/add_content', methods=['POST'])
 def add_content():
@@ -90,12 +91,12 @@ def add_content():
     Input is a json containing two fields
 
     :content_id: str
-    :embedding:  List[float]
+    :content_body:  Union[str, Image]
 
     """
     start_t           = datetime.now()
     input             = request.get_json()
-    content_embedding = np.array(input['embedding']).astype(np.float32)
+    content_embedding = app.config['ClipEncoder'].encode(input['content_body'])
     content_id        = input['content_id']
     app.config['Indexer'].add_content(content_embedding, content_id)
     end_t             = datetime.now()
@@ -110,14 +111,14 @@ def retrieve():
     """
     Input is a json containing two fields
 
-    :query:                  List[float]
+    :query:                  Union[str, Image]
     :n_contents_to_retrieve: int
 
     :return: return a payload with the fields 'contents' (List[str]) 
             and 'scores' (List[float])
     """
     input                  = request.get_json()
-    query_embedding        = np.array(input['query']).astype(np.float32)
+    query_embedding        = app.config['ClipEncoder'].encode(input['query'])
     k                      = input['n_contents_to_retrieve']
     contents, similarities = app.config['Indexer'].retrieve(query_embedding, k)
     return jsonify({'contents': contents, 'scores': similarities.tolist()})
