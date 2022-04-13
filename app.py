554729c2ea66dad45aeccaf503062c238202ab04
links-ai <mediaverse@venus>
2022-04-13 17:51:58 +0200

    updated usage with skimage data

--- app.py
+++ app.py
@@ -8,47 +8,74 @@ Doubts:
     - Faiss assigns an internal ID to each embedding in the `index` structure. We should build a mapping to the global ID of the content within the MV network
 """
 
-
-import argparse
 import logging
-import os
-from typing import List, Tuple, Union
+from typing import List, Tuple
 from datetime import datetime
 
 import faiss
 import numpy as np
 from flask import Flask, jsonify, request
-from logstash_formatter import LogstashFormatterV1
+#from logstash_formatter import LogstashFormatterV1
 
 import torch
 import clip
 from PIL import Image
 from io import BytesIO
-import base64
+from collections import defaultdict
 
 app     = Flask(__name__)
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 EMB_SIZE = 512
+CONTAINER = './contents'
 
 def colored(r, g, b, text):
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
+
+class Container:
+    def __init__(self) -> None:
+        """
+        content is a dictionary of
+        key: embedding
+        value: content id in mv
+        """
+        self.content = defaultdict(lambda: 'Not Present')
+
+    def get_embeddings(self):
+        return self.content.keys()
+
+    def get_mvID(self, embedding):
+        return self.content[embedding]
+
+    def add_content(self, embedding, id):
+        self.content[embedding] = id
 
 class ClipEncoder:
     def __init__(self) -> None:
         model, preprocess = clip.load("ViT-B/32") # (or load model starting from state_dict)
         self.model = model
+        self.model.to(DEVICE).eval()
         self.preprocess = preprocess
 
     def encode(self, input: str, type: str) -> np.array:
+        """
+        moment: 'query' - retrieval
+        """
         if type == 'text':
+            input = input.decode('UTF-8')
             text = clip.tokenize(input).to(DEVICE)
-            return self.model.encode_text(text).detach().cpu().numpy().reshape(512).astype(np.float32)
+            with torch.no_grad():
+                embedding = self.model.encode_text(text).detach().cpu().numpy().reshape(512).astype(np.float32)
         elif type == 'image':
-            image = self.preprocess(Image.open(BytesIO(base64.b64decode(input)))).unsqueeze(0).to(DEVICE)  # b64 input
-            return self.model.encode_image(image).detach().cpu().numpy().reshape(512).astype(np.float32)
-        raise  ValueError(colored(255,0,0, 'Not valid type value, enter text or image'))
+            input = BytesIO(input)
+            image = Image.open(input).convert('RGB')
+            image = self.preprocess(image).unsqueeze(0).to(DEVICE)
+            with torch.no_grad():
+                embedding = self.model.encode_image(image).detach().cpu().numpy().reshape(512).astype(np.float32)
+        else: 
+            raise  ValueError(colored(255,0,0, 'Not valid type value, enter text or image'))
 
-
+        return embedding
+        
 class Indexer:
     def __init__(self, emb_size: int=EMB_SIZE) -> None:
         # to get total length of flat index: index.xb.size()
@@ -56,7 +83,7 @@ class Indexer:
         self.index        = faiss.index_factory(emb_size, "Flat", faiss.METRIC_INNER_PRODUCT)
         self.faissId2MVId = []
 
-    def add_content(self, content_embedding: np.array, content_id: str) -> None:  # (input: np.ndarray or str)
+    def add_content(self, content_embedding: np.array, content_id: str, type: str) -> None:  # (input: np.ndarray or str)
         """
         :param content_embedding: embedding having shape (N, EMB_SIZE)
         """
@@ -66,6 +93,7 @@ class Indexer:
         faiss.normalize_L2(content_embedding)
         self.index.add(content_embedding)
         self.faissId2MVId.append(content_id)
+        
 
     def retrieve(self, query_embedding: np.array, k: int) -> Tuple[List[str], List[float]]:  # (input: np.ndarray or str)
         """
@@ -78,16 +106,12 @@ class Indexer:
         # assuming only one query
         similarities               = similarities[0]
         contents_idx               = contents_idx[0] #faiss internal indices
-        #faiss results are already sorted from highest to lowest
-        #sorted_idx               = np.argsort(similarity)[::-1]
-        #mv_content_ids           = [self.faissId2MVId[idx] for idx in contents_idx[sorted_idx]]
-        #sorted_similarities      = similarity[sorted_idx]
         mv_content_ids             = [self.faissId2MVId[idx] for idx in contents_idx]
-        
         return mv_content_ids, similarities
 
 app.config['Indexer'] = Indexer()
 app.config['ClipEncoder'] = ClipEncoder()
+app.config['Container'] = Container()
 
 @app.route('/mv_retrieval/v0.1/add_content', methods=['POST'])
 def add_content():
@@ -99,11 +123,24 @@ def add_content():
     :type (text or image): str
 
     """
+
     start_t           = datetime.now()
-    input             = request.get_json()
-    content_embedding = app.config['ClipEncoder'].encode(input['content_body'], input['type'])
-    content_id        = input['content_id']
-    app.config['Indexer'].add_content(content_embedding, content_id)
+
+    content           = request.data
+    content_id        = request.args['id']
+    type              = request.args['type']
+
+    content_embedding = app.config['ClipEncoder'].encode(content, type)
+
+    if tuple(content_embedding) in app.config['Container'].get_embeddings():
+        elapsed           = (datetime.now()-start_t).total_seconds()
+        out_msg           = {'msg': 'Content arleady present in the MV archive with id: {}'.format(app.config['Container'].get_mvID(tuple(content_embedding))),
+                            'time': elapsed} 
+        return jsonify(out_msg), 200
+    
+    app.config['Container'].add_content(tuple(content_embedding), content_id)
+
+    app.config['Indexer'].add_content(content_embedding, content_id, type)
     end_t             = datetime.now()
     elapsed           = (end_t-start_t).total_seconds()
     out_msg           = {'msg': 'Content {} successfully added to the indexer'.format(content_id),
@@ -123,8 +160,10 @@ def retrieve():
     :return: return a payload with the fields 'contents' (List[str]) 
             and 'scores' (List[float])
     """
-    input                  = request.get_json()
-    query_embedding        = app.config['ClipEncoder'].encode(input['query'], input['type'])
-    k                      = input['n_contents_to_retrieve']
+    content           = request.data
+    k                 = int(request.args['k'])
+    type              = request.args['type']
+
+    query_embedding        = app.config['ClipEncoder'].encode(content, type)
     contents, similarities = app.config['Indexer'].retrieve(query_embedding, k)
     return jsonify({'contents': contents, 'scores': similarities.tolist()})
--- usage.py
+++ usage.py
@@ -1,23 +1,67 @@
 from flask import json
 import numpy as np
 import requests
-import pdb
+import os
+import random
+import string
 #import time
+
+import skimage
 
 update_url   = 'http://localhost:6000/mv_retrieval/v0.1/add_content'
 retrieve_url = 'http://localhost:6000/mv_retrieval/v0.1/retrieve_contents'
 
+descriptions = {
+    "page": "a page of text about segmentation",
+    "chelsea": "a facial photo of a tabby cat",
+    "astronaut": "a portrait of an astronaut with the American flag",
+    "rocket": "a rocket standing on a launchpad",
+    "motorcycle_right": "a red motorcycle standing in a garage",
+    "camera": "a person looking at a camera on a tripod",
+    "horse": "a black-and-white silhouette of a horse", 
+    "coffee": "a cup of coffee on a saucer"
+}
+
 if __name__ == '__main__':
-    pool  = ["Fabio goes to the market", "Tommaso on the roof", "A dog on the street"]
-    query = "Fabio back from the market"
-    import random
-    import string
+    texts  = list(descriptions.values())[1:]
     
-    for content in pool:
-        random_str = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 10))    
-        payload    = {'content_id': random_str, 'content_body': content}
-        response   = requests.post(update_url,json=payload)
+    # LOAD TEXT
+    # for content in texts:
+    #     random_str = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 10))
+    #     arg = '?id='+random_str+'&type=text'
+
+    #     with open('tmpfile.txt', 'w') as f:
+    #         f.write(content)
+    #     with open('tmpfile.txt', 'rb') as f:
+    #         data = f.read()
+        
+    #     response   = requests.post(update_url+arg, data=data)
+    #     print('[UPDATE] -- status: {}, {}'.format(response.status_code, response.content))
+
+    # LOAD IMAGES
+    for filename in [filename for filename in os.listdir(skimage.data_dir) if filename.endswith(".png") or filename.endswith(".jpg")]:
+        name = os.path.splitext(filename)[0]
+        if name not in descriptions:
+            continue
+        
+        with open(os.path.join(skimage.data_dir, filename), 'rb') as f:
+            data = f.read()
+
+        random_str = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 10))
+        if name == 'page':
+            print(descriptions[name], '(image) :', random_str)
+        arg = '?id='+random_str+'&type=image'
+        response   = requests.post(update_url+arg, data=data)
         print('[UPDATE] -- status: {}, {}'.format(response.status_code, response.content))
-    payload  = {'query': query, 'n_contents_to_retrieve': 1}
-    response = requests.post(retrieve_url, json=payload)
+
+    # QUERY
+    query = "a page of text about segmentation"
+    print('QUERY:', query)
+    with open('tmpfile.txt', 'w') as f:
+        f.write(query)
+    with open('tmpfile.txt', 'rb') as f:
+        data = f.read()
+
+    arg = '?type=text&k=10'
+    response = requests.post(retrieve_url+arg, data=data)
     print('[RETRIEVE]-- status: {}, {}'.format(response.status_code, response.content))
